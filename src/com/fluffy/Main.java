98a41f4d422b24c98dc1f5ab61b2125e701153fa
Vadim Sivokon <vadim.sivokon.vs@gmail.com>
2022-11-19 13:07:11 +0200

    Add Main class

---
+++ src/com/fluffy/Main.java
@@ -0,0 +1,16 @@
+package com.fluffy;
+
+public class Main {
+    /**
+     * Tests all class methods.
+     */
+    public static void main(String[] args) {
+        // TODO: add tests here
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItem("Apple", 0.99, 5, ShoppingCart.ItemType.NEW);
+        cart.addItem("Banana", 20.00, 4, ShoppingCart.ItemType.SECOND_FREE);
+        cart.addItem("A long piece of toilet paper", 17.20, 1, ShoppingCart.ItemType.SALE);
+        cart.addItem("Nails", 2.00, 500, ShoppingCart.ItemType.REGULAR);
+        System.out.println(cart.formatTicket());
+    }
+}
--- src/com/fluffy/ShoppingCart.java
+++ src/com/fluffy/ShoppingCart.java
@@ -13,19 +13,6 @@ public class ShoppingCart {
     public enum ItemType { NEW, REGULAR, SECOND_FREE, SALE }
 
     /**
-     * Tests all class methods.
-     */
-    public static void main(String[] args) {
-        // TODO: add tests here
-        ShoppingCart cart = new ShoppingCart();
-        cart.addItem("Apple", 0.99, 5, ItemType.NEW);
-        cart.addItem("Banana", 20.00, 4, ItemType.SECOND_FREE);
-        cart.addItem("A long piece of toilet paper", 17.20, 1, ItemType.SALE);
-        cart.addItem("Nails", 2.00, 500, ItemType.REGULAR);
-        System.out.println(cart.formatTicket());
-    }
-
-    /**
      * Adds new item.
      *
      * @param title    item title 1 to 32 symbols
